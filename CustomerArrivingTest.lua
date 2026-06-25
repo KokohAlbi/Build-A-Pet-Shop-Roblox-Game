4612e9237652c696edd7389905df35e09946aab9
KokohAlbi <albijaphet01@gmail.com>
2026-06-25 23:47:58 +0700

    Lua script for testing customer arriving

---
+++ CustomerArrivingTest.lua
@@ -0,0 +1,12 @@
+local npc = script.Parent
+local humanoid = npc:WaitForChild("Humanoid")
+local target = game.Workspace:WaitForChild("NPC kasir")
+task.wait(10)
+humanoid:MoveTo(target.Position)
+humanoid.MoveToFinished:Connect(function(reached)
+	if reached then
+		print("NPC telah sampai di tujuan.")
+	else
+		print("NPC gagal mencapai tujuan.")
+	end
+end)
