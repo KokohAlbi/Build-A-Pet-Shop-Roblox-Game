052954d874b9bffcbb743dc0214359c3fe43132d
KokohAlbi <albijaphet01@gmail.com>
2026-06-25 23:46:06 +0700

    Lua script for cashbar

---
+++ CashBarScript.lua
@@ -0,0 +1,10 @@
+local player = game.Players.LocalPlayer
+local leaderstats = player:WaitForChild("leaderstats")
+local cash = leaderstats:WaitForChild("Cash")
+local cashText = script.Parent:WaitForChild("CashBar"):WaitForChild("CashText")
+local function updateCashUI(newAmount)
+	cashText.Text = "$" .. tostring(newAmount)
+end
+
+updateCashUI(cash.Value)
+cash.Changed:Connect(updateCashUI)
